5eb70f5b0b66cfcf04b2d383d999d02928f629f6
Kenneth Laws <kenneth.laws@here.com>
2018-03-06 15:24:24 -0800

    adds bin build

---
+++ code/build/CMakeFiles/velodyne_node.dir/DependInfo.cmake
@@ -0,0 +1,14 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/earthmine/Vishi/real_time_lidar/VelodyneMonitor.cpp" "/home/earthmine/Vishi/real_time_lidar/build/CMakeFiles/velodyne_node.dir/VelodyneMonitor.o"
+  "/home/earthmine/Vishi/real_time_lidar/velodyne_node.cpp" "/home/earthmine/Vishi/real_time_lidar/build/CMakeFiles/velodyne_node.dir/velodyne_node.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ code/build/CMakeFiles/velodyne_node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/velodyne_node.dir/velodyne_node.o"
+  "CMakeFiles/velodyne_node.dir/VelodyneMonitor.o"
+  "../bin/velodyne_node.pdb"
+  "../bin/velodyne_node"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/velodyne_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
